56463d0928078c0dcb5d322f344855a4d45a8f50
Wonjun Lee <wonjunlee@Wonjuns-MacBook.local>
2016-09-01 16:41:25 -0400

    updated

--- mysql-to-txt.py
+++ mysql-to-txt.py
@@ -1,9 +1,17 @@
 from pprint import pprint
 from tools import parsing_document_contents
 
+##########################################
+#				INPUT
+##########################################
+
+sql_file_path = u"../example.txt"
+output_path = u"../parsed_documents"
+
+##########################################
 # Open the file and load it into a string
 text = ""
-with open(u"c:/users/wlee/downloads/example.txt","r") as F:
+with open(sql_file_path,"r") as F:
 	for line in F:
 		line = unicode(line, encoding='utf-8', errors='ignore')
 		text+=line.strip()
@@ -14,29 +22,17 @@ text = text.split(u"INSERT INTO")
 # Parsing document_contents
 parsed_document_contents = {}
 for i in range(len(text)):
-	if i > 0 and i < len(text) - 1:
-		parsed_document_contents = parsing_document_contents(text[i], parsed_document_contents)
+	parsed_document_contents = parsing_document_contents(text[i], parsed_document_contents)
 
-for key in parsed_text.keys():
+# Print the number of Items
+for key in parsed_document_contents.keys():
 	print key
-	print "Number of items:", len(parsed_text[key])
-
-# for key, values in parsed_text.iteritems():
-# 	value_ind = 0
-# 	for value in values:
-# 		try:
-# 			a = int(value[0].split(",")[0])
-# 		except:
-# 			print key
-# 			print value
-# 			print value_ind
-# 		value_ind += 1
+	print "Number of items:", len(parsed_document_contents[key])
 
 # Save articles to txt files
 import re
-for key, value in parsed_text.iteritems():
-	folder = "documents"
+for key, value in parsed_document_contents.iteritems():
 	for article in value:
-		with open(u"c:/users/wlee/downloads/{}/{}.txt".format(folder, article[0]), "w") as F:
+		with open(u"{}/{}.txt".format(output_path, article[0]), "w") as F:
 			F.write(article[1])
 
--- tools.py
+++ tools.py
@@ -1,7 +1,10 @@
-# Parse sql script into dictionary
+""" Parse sql script into dictionary """
 
 # Parsing document_contents database
 def parsing_document_contents(text, parsed_document_contents):
+	if u'document_contents' not in text[:50]:
+		return parsed_document_contents
+
 	text = text.split(u"\'")
 	for i in text:
 		if u'VALUES' in i:
@@ -9,7 +12,7 @@ def parsing_document_contents(text, parsed_document_contents):
 			table = table[2:len(table)-2]
 			ID = i.split(u'(')[-1][:-1]
 			content = ""
-			if table not in parsed_text.keys():
+			if table not in parsed_document_contents.keys():
 				parsed_document_contents[table] = []
 		elif u"),(" in i:
 			if i.split(u'),(')[-1][0].isdigit():
@@ -19,34 +22,3 @@ def parsing_document_contents(text, parsed_document_contents):
 		else:
 			content += i
 	return parsed_document_contents
-
-"""
-	(586611,
-		'Voodoo in Haitian Society',
-		'voodoo-haitian-society',
-		336,1,1,1,
-		'Haiti,United States,
-		West Africa,
-		2010 Haiti earthquake,Dominican Republic',
-		0,
-		'active',
-		'WW',
-		209158,
-		'Unpublished',
-		0
-		),
-	(586612,
-		'kazakh',
-		'kazakh',
-		1828,7,1,4,
-		'United States,China,
-		Christian values,
-		Working class,
-		Asia-Pacific Economic Cooperation',
-		0,
-		'active',
-		'WW',
-		209159,
-		'Unpublished',
-		0)
-"""
